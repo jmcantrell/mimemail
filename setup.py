5b75857cca25c4dbc3b6e778d999df1bc1c688a0
Jeremy Cantrell <jmcantrell@gmail.com>
2019-10-30 00:11:59 -0500

    Change formatting to meet python style recommendations

--- setup.py
+++ setup.py
@@ -3,33 +3,41 @@
 from setuptools import setup
 
 setup(
-        name='MIMEMail',
-        version='0.4.6',
-        description='Command line MUA using the MIME message format.',
-        author='Jeremy Cantrell',
-        author_email='jmcantrell@gmail.com',
-        classifiers=[
-            'Development Status :: 4 - Beta',
-            'Environment :: Console',
-            'Intended Audience :: System Administrators',
-            'License :: OSI Approved :: BSD License',
-            'Natural Language :: English',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python',
-            'Topic :: Communications :: Email :: Email Clients (MUA)',
-            'Topic :: System :: Systems Administration',
-            'Topic :: Utilities',
-            ],
-        install_requires=[
-            'UnicodeUtils',
-            'ScriptUtils',
-            ],
-        entry_points={
-            'console_scripts': [
-                'mimemail=mimemail:main',
-                ]
-            },
-        py_modules=[
-            'mimemail',
-            ],
-        )
+
+    name='mimemail',
+    version='0.4.6',
+
+    description='Command line MUA using the MIME message format.',
+
+    author='Jeremy Cantrell',
+    author_email='jmcantrell@gmail.com',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'License :: Public Domain',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Communications :: Email :: Email Clients (MUA)',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities',
+    ],
+
+    install_requires=[
+        'unicodeutils',
+        'scriptutils',
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'mimemail=mimemail:main',
+        ]
+    },
+
+    py_modules=[
+        'mimemail',
+    ],
+
+)
